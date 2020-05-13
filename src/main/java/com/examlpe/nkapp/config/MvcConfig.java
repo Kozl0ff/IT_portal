cbfd039285df13b4615aad1c5f087ce26b4db632
nikita <nik.k200181@gmail.com>
2020-05-13 19:53:02 +0400

    Version 1.0.0

--- src/main/java/com/examlpe/nkapp/config/MvcConfig.java
+++ src/main/java/com/examlpe/nkapp/config/MvcConfig.java
@@ -1,8 +1,10 @@
 package com.examlpe.nkapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
@@ -15,9 +17,18 @@ public class MvcConfig implements WebMvcConfigurer {
         freeMarkerViewResolver.setSuffix(".ftl");
         return freeMarkerViewResolver;
     }
+    @Value("${upload.path}")
+    private String uploadPath;
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file://" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
